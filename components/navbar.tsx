9de02628bd0b92fe154477c62e0ca32c10d9fff0
taaziz1 <taaziz.b@gmail.com>
2026-04-21 23:39:28 -0700

    add admin link to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,9 +2,37 @@ import Link from "next/link";
 import { Suspense } from "react";
 import { AuthButton } from "@/components/auth-button";
 import { hasEnvVars } from "@/lib/utils";
-import { EnvVarWarning } from "@/components/env-var-warning";
+import { EnvVarWarning } from "@/components/env-var-warning"; 
+import { createClient } from "@/lib/supabase/server";
+
+async function AdminLink() {
+      const supabase = await createClient();
+
+      const { data, error } = await supabase.auth.getClaims();
+      const user = data?.claims;
+
+      const { data: profile } = await supabase
+      .from("users")
+      .select("display_name, username, is_admin")
+      .eq("id", user?.sub)
+      .single();
+    
+    if (profile?.is_admin) {
+      return (
+        <Link
+              href="/protected/admin"
+              className="text-muted-foreground hover:text-primary transition-colors"
+            >
+              Admin Panel
+        </Link>
+      )
+    }
+    return null;
+    
+}
 
 export function Navbar() {
+
   return (
     <nav className="w-full border-b border-border/30 bg-card/50 backdrop-blur-sm">
       <div className="w-full max-w-6xl mx-auto flex items-center justify-between h-16 px-5">
@@ -42,6 +70,10 @@ export function Navbar() {
           >
             Leaderboard
           </Link>
+          <Suspense fallback={null}>
+                <AdminLink/>
+              </Suspense>
+          
         </div>
 
         {/* Auth */}
